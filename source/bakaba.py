afa83185ac71ade70602f1b0e3751483d2c59295
Jacobus Burger <therealjacoburger@gmail.com>
2022-01-13 14:14:56 -0800

    Fixed typos.

--- source/bakaba.py
+++ source/bakaba.py
@@ -2,21 +2,20 @@
 # Created by Jacobus Burger (2022)
 # Info:
 #   I had a shower thought about triangular repeating words like
-#   bakaba, bakakakaba, etc, maintaining soem symmetry no matter how
-#   long it might be. I wondered if there was and algorithm to accompish
+#   bakaba, bakakakaba, etc, maintaining some symmetry no matter how
+#   long it might be. I wondered if there was an algorithm to accompish
 #   this dumb passing thought.
 #
 #   here is my attempt at that.
 
 
 def bakaba(repetitions, string):
-    part = string[2 : len(string) - 2]
-    return string[:2] + part * repetitions + string[len(string) - 2 :]
+    ka = string[2 : len(string) - 2]
+    return string[:2] + ka * repetitions + string[len(string) - 2 :]
 
 
 if __name__ == "__main__":
     while True:
         string = input("what string like 'bakaba' do you want to repeat? ")
         repetitions = int(input("how many times should the middle repeat? "))
-        # put what you would like here
         print(bakaba(repetitions, string))
