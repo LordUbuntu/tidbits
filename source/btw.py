73bd23efba8cb9867c3785d414cb057f5194dbbf
Jacobus Burger <therealjacoburger@gmail.com>
2022-06-11 19:27:39 -0700

    had some short fun

---
+++ source/btw.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+# Created by Jacobus Burger (2022)
+# Info:
+#   I was testing that nerdfont works, I thought it would be funny to test
+#   with a classic "I use arch btw" statement. Ended up with this silly
+#   little script.
+def main():
+    symbol = input() or ''
+    print(f"I use {symbol} btw ;)")
+
+
+if __name__ == '__main__':
+    main()
