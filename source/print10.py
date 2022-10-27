a76140f139fd9d520632be067373861b53395852
Jacobus Burger <therealjacoburger@gmail.com>
2022-10-27 16:15:38 -0700

    add tidbit print10

---
+++ source/print10.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+# Jacobus Burger (2022)
+# Info:
+#   The famous Commodor 64 Basic program that generated a neat pattern
+#   Done in python.
+from random import choice
+char = ["\\", "\/"]
+while True:
+    print(choice(char), end="")
