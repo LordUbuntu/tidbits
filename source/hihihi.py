efef8a3ba990d60d84fdcbd3f60346874563f933
Jacobus Burger <therealjacoburger@gmail.com>
2022-10-30 21:27:13 -0700

    added hihihi tidbit

---
+++ source/hihihi.py
@@ -0,0 +1,9 @@
+# Jacobus Burger (2002)
+# Info:
+#   Recreation of the feeling of writing the first computer
+#   my first computer program. I don't remember what that program
+#   was or what it did, but I recalled the feeling of wonder and
+#   excitement from that time in a dream, and wanted to take this
+#   moment to celebrate it.
+while True:
+    print("hi", end=' ')
