54f8c0fab950af1cb9a4d966d7d5c626abb42d1b
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-09 19:53:02 -0800

    added number guessing game

---
+++ guess.py
@@ -0,0 +1,13 @@
+from random import randint as rand
+
+
+if __name__ == '__main__':
+    number = rand(1, 100)
+    while True:
+        guess = int(input("Guess a number: "))
+        if guess == number:
+            print("You did it!")
+        if guess > number:
+            print("Higher...")
+        if guess < number:
+            print("Lower...")
