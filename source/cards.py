66c493421b4db3a89406b5a3aec7f32d6d5c3aa8
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-14 17:09:26 -0800

    added output formatter

--- source/cards.py
+++ source/cards.py
@@ -13,6 +13,38 @@ cards = [card for card in range(13)]
 suits = [suit for suit in range(4)]
 
 
+def print_card(card, suit: int):
+    string = ""
+
+    # determine the card type
+    if card == 0:
+        string += "Ace "
+    elif 1 <= card <= 8:
+        string += f"{card[0] + 1} "
+    elif card == 9:
+        string += "Jack "
+    elif card == 10:
+        string += "Queen "
+    elif card == 11:
+        string += "King "
+    else:
+        string += "Aster "  # something's gone wrong
+
+    # determine the card suit
+    if suit == 0:
+        string += "of Clubs"
+    if suit == 1:
+        string += "of Hearts"
+    if suit == 2:
+        string += "of Clovers"
+    if suit == 3:
+        string += "of Diamonds"
+    else:
+        string += "the King in Yellow"  #something's gone wrong
+
+    return string
+
+
 def game(draws: int):
     pass
 
