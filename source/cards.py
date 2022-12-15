24db515481195234ed0997f825c6c4a2af9f02a7
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-14 17:23:51 -0800

    successfully implemented card game

--- source/cards.py
+++ source/cards.py
@@ -8,7 +8,7 @@ from random import choice, randint
 # EG: Ace of Clubs == (0, 0)
 # Ace, 2, 3, 4, 5, 6, 7, 8, 9, J,  K,  Q
 # 0,   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-cards = [card for card in range(13)]
+cards = [card for card in range(12)]
 # Club, Heart, Clover, Diamond
 # 0,    1,     2,      3
 suits = [suit for suit in range(4)]
@@ -21,15 +21,13 @@ def show_card(card, suit: int):
     if card == 0:
         string += "Ace "
     elif 1 <= card <= 8:
-        string += f"{card[0] + 1} "
+        string += f"{card + 1} "
     elif card == 9:
         string += "Jack "
     elif card == 10:
         string += "Queen "
     elif card == 11:
         string += "King "
-    else:
-        string += "Aster "  # something's gone wrong
 
     # determine the card suit
     if suit == 0:
@@ -40,20 +38,20 @@ def show_card(card, suit: int):
         string += "of Clovers"
     if suit == 3:
         string += "of Diamonds"
-    else:
-        string += "the King in Yellow"  #something's gone wrong
 
     return string
 
 
 def game(draws: int):
-    guess = [randint(len(cards)), randint(len(suits))]  # [card, suit]
+    guess = [randint(0, len(cards) - 1), randint(0, len(suits) - 1)]  # [card, suit]
     for _ in range(draws):
         card, suit = choice(cards), choice(suits)
         print(show_card(card, suit))
         if card == guess[0] and suit == guess[1]:
             print("Congatrulaions! You got the winning card!\n\n")
             quit()
+    else:
+        print("You lose!")
 
 
 if __name__ == '__main__':
