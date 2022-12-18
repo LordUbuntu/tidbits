ba8e0cc5480461dbb657d8872153ee265e9ec7c7
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-17 17:24:04 -0800

    added randomized capitalization vapourtext

--- source/vapourtext.py
+++ source/vapourtext.py
@@ -2,6 +2,8 @@
 # Created by Jacobus Burger (2022)
 # Info:
 #   Code golf to print vapour-ware strings eg "h e l l o"
+from random import randint as rnd
+
 
 def vapourize(sentence):
     return ' '.join([*sentence])
@@ -10,7 +12,12 @@ def vapourize(sentence):
 def VAPOURIZE(sentence):
     return ' '.join([*sentence.upper()])
 
+
+def random_vapourize(sentence):
+    return ' '.join([char if rnd(0, 1) else char.lower() for char in sentence.upper()])
+
 if __name__ == '__main__':
     sentence = input()
     print(vapourize(sentence))
     print(VAPOURIZE(sentence))
+    print(random_vapourize(sentence))
