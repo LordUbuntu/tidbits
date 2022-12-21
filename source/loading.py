1cae484b2b39f8d9394960f786aff4805d885e5e
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-21 15:50:56 -0800

    added sweep animation and improved prompt experience

--- source/loading.py
+++ source/loading.py
@@ -36,22 +36,33 @@ def orb(speed):
     print()
 
 
-if __name__ == '__main__':
-    choice = int(input(
-            """Choose animation:
-                (1) %
-                (2) loading...
-                (3) /
-                (4) O
-            """
-    ))
-    speed = int(input("What speed? "))
-    if choice == 1:
-        percent(speed)
-    if choice == 2:
-        loading(speed)
-    if choice == 3:
-        spin(speed)
-    if choice == 4:
-        orb(speed)
+def sweep(speed):
+    phase = ["*..", ".*.", "..*", "..."]
+    for i in range(0, 101, 3):
+        print(phase[i % 4], end='\r')
+        sleep(1 / speed)
+    print()
 
+
+if __name__ == '__main__':
+    while True:
+        choice = int(input(
+                """Choose animation:
+                    (1) percent
+                    (2) loading
+                    (3) spin
+                    (4) orb
+                    (5) sweep
+                """
+        ))
+        speed = int(input("What speed? "))
+        if choice == 1:
+            percent(speed)
+        if choice == 2:
+            loading(speed)
+        if choice == 3:
+            spin(speed)
+        if choice == 4:
+            orb(speed)
+        if choice == 5:
+            sweep(speed)
