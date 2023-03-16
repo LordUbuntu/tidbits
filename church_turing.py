1613aa2bb416b80e173e32d1cee7d95804bae11b
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-16 10:24:14 -0700

    added beginnings of church-turing thesis demo

---
+++ church_turing.py
@@ -0,0 +1,10 @@
+# Jacobus Burger (2023)
+# Demonstration of the Church-Turing thesis as it relates to iteration.
+# Any given loop can be expressed as a recursive function, and vice-versa
+
+
+def rloop(i = 0, f, *args):
+    if i <= 0:
+        return
+    f(args[0])
+    return rloop(i - 1, f, *args[1:])
