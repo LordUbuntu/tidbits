8f5555e21990f04dcbf06d77501a4922523e4752
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-29 13:39:49 -0700

    improved rot13 code with better indexing and math

--- rot13.py
+++ rot13.py
@@ -5,12 +5,12 @@
 #   is encoded using a repeatedly shifting rotating encoder or
 #   some other dynamically shifting encoding but that's just an
 #   opinion until proven otherwise.
+from string import ascii_lowercase
 
+
+# note: only takes strings with no spaces or any non-letter ascii
 def rot13(data: str) -> str:
-    result = []
-    for char in data.upper():
-        result.append( chr(((ord(char) - 65 + 13) % 25) + 65) )
-    return result
+    return ''.join(ascii_lowercase[(i + 13) % 26] for i in map(ascii_lowercase.index, data.lower()))
 
 
 if __name__ == '__main__':
