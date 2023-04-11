88481c0af6d22a578e7ce3a7170d2e22231a2c7b
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-10 20:17:51 -0700

    added scat tidbit

---
+++ scat.py
@@ -0,0 +1,28 @@
+# Jacobus Burger (2023)
+# Info:
+#   Inspired by listening to some Cab Calloway, decided to make a program
+#   to convert a string to scat with decent flow. While working on it
+#   I realized that this is a decently complex problem.
+
+
+def scat(string: str) -> str:
+    scats = {
+        'b': "bap",
+        'd': "dee",
+        'e': "ee",
+        'f': "fee",
+        'h': "hee",
+        'j': "jack",
+        'k': "kap",
+        'l': "lee",
+        'm': "mo",
+        'n': "nap",
+        'o': "oo",
+        'p': "pah",
+        'r': "rum",
+        's': "scat",
+        't': "tat",
+        'v': "vee",
+        'w': "wee",
+        'z': "zap"
+    }
