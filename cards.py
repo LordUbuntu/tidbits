1e6e1a2926bbeef62922ea39dd57e523e4e11d9c
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-12 15:37:14 -0700

    changed game

--- cards.py
+++ cards.py
@@ -24,41 +24,21 @@ cards = [
 suits = ["Club", "Heart", "Clover", "Diamond"]
 
 
-def show_card(card, suit: int):
-    string = ""
-    # determine the card type
-    if card == 0:
-        string += "Ace "
-    elif 1 <= card <= 8:
-        string += f"{card + 1} "
-    elif card == 9:
-        string += "Jack "
-    elif card == 10:
-        string += "Queen "
-    elif card == 11:
-        string += "King "
-    # determine the card suit
-    if suit == 0:
-        string += "of Clubs"
-    if suit == 1:
-        string += "of Hearts"
-    if suit == 2:
-        string += "of Clovers"
-    if suit == 3:
-        string += "of Diamonds"
-    return string
+def get_card():
+    return ' '.join([choice(cards), "of", choice(suits)])
 
 
 def game(draws: int):
-    guess = [randint(0, len(cards) - 1), randint(0, len(suits) - 1)]  # [card, suit]
+    answer = ' '.join([choice(cards), "of", choice(suits)])
+    print("=== Winning Card is ", answer, "===")
     for _ in range(draws):
-        card, suit = choice(cards), choice(suits)
-        print(show_card(card, suit))
-        if card == guess[0] and suit == guess[1]:
-            print("Congatrulaions! You got the winning card!\n\n")
-            quit()
-    else:
-        print("You lose!")
+        card = get_card()
+        print("You got ", card)
+        if card == answer:
+            print("You won!!")
+            break
+        else:
+            print("Try again...")
 
 
 if __name__ == '__main__':
