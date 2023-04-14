da2faf8c9c171be972b4e63e2469459a6b6b6f17
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-13 22:52:35 -0700

    added cute joke

---
+++ line.py
@@ -0,0 +1,8 @@
+# Jacobus Burger (2023)
+# Info:
+#   This is it, somebody has to draw the line somewhere!!1! >:(
+#   Sorry :^) (not sorry).
+from turtle import Turtle
+t = Turtle()
+t.pencolor("red")
+t.forward(100)
