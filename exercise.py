fb38877038cd1d83c1e917e4284a88077fdbbeca
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-15 22:46:14 -0700

    added exercise generator

--- exercise.py
+++ exercise.py
@@ -4,3 +4,22 @@
 #   This may become a ChatGPT + Exercise Finder + React application
 # See:
 #   https://whitecoattrainer.com/blog/best-exercises
+from random import choice, randint
+
+
+EXERCISES = [
+    # legs
+    "Pistols",
+]
+
+
+def exercise():
+    return ' '.join(
+        [
+            randint(1,5),      # number of sets
+            "by",
+            randint(1,15),       # number of reps
+            "of",
+            choice(EXERCISES)   # exercise
+        ]
+    )
