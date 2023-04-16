1524a019309de9ab2171ae0eee405929f23969da
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-15 22:38:22 -0700

    added new tidbit

---
+++ exercise.py
@@ -0,0 +1,6 @@
+# Jacobus Burger (2023)
+# Info:
+#   Randomly reccomend some exercises in a routine with random numbers.
+#   This may become a ChatGPT + Exercise Finder + React application
+# See:
+#   https://whitecoattrainer.com/blog/best-exercises
