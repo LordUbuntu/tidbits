19110d76a287af9062fa90ae677bd12163c937ce
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-21 21:38:33 -0700

    removed base64 in C, very complicated and it's late

--- base64.c
+++
@@ -1,7 +0,0 @@
-/* Jacobus Burger (2023)
- * Info:
- *      A program to convert base-64 strings into ascii, to translate
- *      the codes in a recent video
- * See:
- *      https://www.youtube.com/watch?v=bOCHTHkBoAs
- */
