864d25081099dfb0341bc351aa428fccf1339b16
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-21 20:42:31 -0700

    started base-64 program

---
+++ base64.c
@@ -0,0 +1,7 @@
+/* Jacobus Burger (2023)
+ * Info:
+ *      A program to convert base-64 strings into ascii, to translate
+ *      the codes in a recent video
+ * See:
+ *      https://www.youtube.com/watch?v=bOCHTHkBoAs
+ */
