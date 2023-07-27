229352df3b794b90f1e7f1c087c8546321af2e97
LordUbuntu <therealjacoburger@gmail.com>
2023-07-27 16:32:36 -0700

    added randomized rotation

--- rot13.py
+++ rot13.py
@@ -5,6 +5,7 @@
 #   is encoded using a repeatedly shifting rotating encoder or
 #   some other dynamically shifting encoding but that's just an
 #   opinion until proven otherwise.
+from random import randint as random
 from string import ascii_lowercase
 
 
@@ -13,6 +14,13 @@ def rot13(data: str) -> str:
     return ''.join(ascii_lowercase[(i + 13) % 26] for i in map(ascii_lowercase.index, data.lower()))
 
 
+def rotN(data: str) -> str:
+    N = random(1, 100)
+    return ''.join(ascii_lowercase[(i + N) % 26] for i in map(ascii_lowercase.index, data.lower()))
+
+
 if __name__ == '__main__':
     string = input()
     print(rot13(string))
+    print(rotN(string))
+    print(rotN(string))
