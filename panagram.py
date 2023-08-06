50d35e27fd1744fcbcbb31f260a73304673962fa
Jacobus Burger <therealjacoburger@gmail.com>
2023-08-05 20:41:31 -0700

    Create panagram.py
    Created a new program to generate panagrams.

---
+++ panagram.py
@@ -0,0 +1,19 @@
+# Jacobus Burger (2023)
+# A program that generates panagrams, maybe could be used to preview fonts without it getting boring?
+from random import choice
+
+def basic_panagram():
+  PANAGRAMS = [
+    "Waltz, bad nymph, for quick jigs vex.",
+    "Glib jocks quiz nymph to vex dwarf.",
+    "Sphinx of black quartz, judge my vow.",
+    "How quickly daft jumping zebras vex!",
+    "The five boxing wizards jump quickly.",
+    "Jackdaws love my big sphinx of quartz.",
+    "Pack my box with five dozen liquor jugs.",
+  ]
+  return choice(PANAGRAMS)
+
+
+if __name__ == '__main__':
+  print(basic_panagram())
