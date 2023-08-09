aba0bb3cd744e4bb637d454dc180c25e1db70b57
LordUbuntu <therealjacoburger@gmail.com>
2023-08-09 15:58:26 -0700

    final: completed tictactoe game

--- tictactoe.py
+++ tictactoe.py
@@ -5,7 +5,7 @@ from os import system, name
 
 
 def display(gamestate):
-    system("cls" if os.name == "nt" else "clear")
+    system("cls" if name == "nt" else "clear")
     for i in range(3):
         print('|'.join(map(
             lambda item: 'X' if item == 1 else
@@ -13,14 +13,7 @@ def display(gamestate):
                          ' ',
             gamestate[i * 3 : i * 3 + 3])))
         print("-+-+-" if i != 2 else "\n\n")
-    print("""
-    REMEMBER
-    1|2|3
-    -+-+-
-    4|5|6
-    -+-+-
-    7|8|9
-    """)
+    print("REMEMBER\n1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9")
     print("\n\n")
 
 
@@ -44,7 +37,7 @@ def winner(gamestate):
         # check for a diagonal line
         if all(state == 1 for state in gamestate[i :: 4]):
             return 1
-        for all(state == 2 for state in gamestate[i :: 4]):
+        if all(state == 2 for state in gamestate[i :: 4]):
             return 2
     return 0
 
@@ -54,9 +47,21 @@ def main():
     while True:
         # update screen
         display(gamestate)
-        # get user input
+        # check who won and announce it
+        outcome = winner(gamestate)
+        if outcome == 1:
+            print("X WINS!!!\n\n")
+            break
+        if outcome == 2:
+            print("O WINS!!!\n\n")
+            break
+        # get player move and set tile to X if open (otherwise skip turn)
         X = int(input("Where should X go? "))
-        # get enemy input
-        O = bot_move(gamestate)
-        # check for winners
-        winner(gamestate)
+        gamestate[X - 1] = 1 if gamestate[X - 1] == 0 else gamestate[X - 1]
+        # select first open tile and set as enemy O
+        O = next(filter(lambda v: v[1] != 1 and v[1] != 2, enumerate(gamestate)))[0]
+        gamestate[O] = 2
+
+
+if __name__ == '__main__':
+    main()
