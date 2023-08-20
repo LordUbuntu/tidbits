4b1c1a59f2dc51222ee376da7eb973277434f2be
LordUbuntu <therealjacoburger@gmail.com>
2023-08-19 21:06:10 -0700

    added pizzaz 🎉 for coming year

--- happy_new_years.py
+++ happy_new_years.py
@@ -4,4 +4,4 @@ from datetime import datetime
 if __name__ == '__main__':
     times = int(input())
     for _ in range(times):
-        print("Happy New Years {}!".format(datetime.now().year))
+        print("🎆 Happy New Years {} 🎉".format(datetime.now().year))
