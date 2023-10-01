3fa74e1ea96477ebf925143cd81e77ed08304569
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-30 21:32:20 -0700

    fix: sentence repetition typo removed

--- hihihi.py
+++ hihihi.py
@@ -1,6 +1,6 @@
 # Jacobus Burger (2002)
 # Info:
-#   Recreation of the feeling of writing the first computer
+#   Recreation of the feeling of writing
 #   my first computer program. I don't remember what that program
 #   was or what it did, but I recalled the feeling of wonder and
 #   excitement from that time in a dream, and wanted to take this
