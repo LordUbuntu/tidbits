f934fb931d8fa3531cebebe1f2ea908625822802
Jacobus Burger <therealjacoburger@gmail.com>
2023-11-14 21:44:21 -0800

    added mega cringe zoomer speak c program to be extra

---
+++ zoom.c
@@ -0,0 +1,30 @@
+/* Jacobus Burger (2023)
+ * Internet zoomer speak for fun
+ */
+#define include finna
+#define cap 0
+#define no !
+#define be =
+#define dobe ==
+#define glowup ++
+#define sus if
+#define mood else
+#define frfr for
+#define smol int
+#define clapback printf
+#define vibecheck scanf
+#define GOAT int main(void)
+
+
+#include <stdio.h>
+
+
+GOAT {
+        char vibe[2];
+        char good be no cap;
+        vibecheck("%s", vibe);
+        sus (vibe dobe "hi" && good dobe no cap)
+                clapback("hey!");
+        frfr (smol i be cap; i <= 10; i glowup)
+                clapback("count is %i\n", i);
+}
