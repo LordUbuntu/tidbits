2f5f5220d8c041f4a24d8b3b3553f5135e7ef168
Jacobus Burger <therealjacoburger@gmail.com>
2023-11-15 23:42:49 -0800

    they said I was mad, but I showed them! >:)

--- zoom.c
+++ zoom.c
@@ -1,30 +1,57 @@
 /* Jacobus Burger (2023)
  * Internet zoomer speak for fun
+ *
+ * They said I was a madman, that it couldn't be done!
+ * Normal C users be confused by this, frfr
  */
 #define include finna
 #define cap 0
+#define bet 1
 #define no !
+#define not !
 #define be =
-#define dobe ==
+#define do_be ==
+#define lowkey <
+#define really >
+#define lowkey_be <=
+#define really_be >=
+#define bruh NULL
 #define glowup ++
+#define glowdown --
+#define rn {
+#define fr }
 #define sus if
-#define mood else
-#define frfr for
-#define smol int
+#define sussy else if
+#define baka else
+#define count for
+#define yeet switch
+#define vibe case
+#define dab return
+#define smol char
+#define innit int
+#define uge long
 #define clapback printf
-#define vibecheck scanf
-#define GOAT int main(void)
+#define tea scanf
+#define comprable strcmp
+#define GOAT int main(int argc, char *argv[])
 
 
 #include <stdio.h>
+#include <string.h>
 
-
-GOAT {
-        char vibe[2];
-        char good be no cap;
-        vibecheck("%s", vibe);
-        sus (vibe dobe "hi" && good dobe no cap)
-                clapback("hey!");
-        frfr (smol i be cap; i <= 10; i glowup)
-                clapback("count is %i\n", i);
-}
+GOAT rn
+        smol mood[5] be "    \0";
+        tea("%4s", mood);
+        clapback("mood do be %s rn\n", mood);
+        
+        sus (comprable("good", mood) do_be cap) rn
+                clapback("yo that's cool bro ngl!\n");
+        fr sussy (comprable("fine", mood) do_be cap) rn
+                clapback("yo bro I gotchu!\n");
+                count (smol i be cap; i lowkey 11; i glowup) rn
+                         clapback("bro fr be a %i\n", i);
+                fr
+        fr baka rn
+                clapback("yo, what?\n");
+        fr
+fr
