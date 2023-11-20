a9b009baaf6bae8272f724d5de65d52c49121d30
Jacobus Burger <therealjacoburger@gmail.com>
2023-11-20 14:58:05 -0800

    remade as a header file instead

---
+++ zoom.h
@@ -0,0 +1,126 @@
+/* Jacobus Burger (2023)
+ * Internet zoomer speak for fun
+ *
+ * They said I was a madman, that it couldn't be done!
+ * Normal C users be confused by this, frfr
+ */
+// constants
+#define bruh NULL
+// booleans
+#define cap 0
+#define bet 1
+#define no !
+#define not !
+#define uh &&
+#define huh ||
+#define do_be ==
+#define no_be !=
+#define lowkey <
+#define really >
+#define lowkey_be <=
+#define really_be >=
+// branch
+#define sus if
+#define sussy else if
+#define baka else
+#define yeet switch
+#define vibe case
+#define boop break
+#define doot default
+#define dab return
+// iteration
+#define count for
+#define bro while
+// symbols
+#define be =
+#define glowup ++
+#define glowdown --
+#define do {
+#define fr }
+#define rn ;
+#define finna :
+// qualifiers
+#define based const
+#define banger static
+#define noodle volatile
+// types
+#define bitty char
+#define smol short
+#define mid int
+#define boujee long
+#define mad void
+// functions
+#include <stdio.h>
+#include <string.h>
+#define clapback printf
+#define say puts
+#define tea scanf
+#define comprable strcmp
+#define longboy strlen
+#define GOAT int main(int argc, char *argv[])
+
+
+
+
+
+/* Example programs:
+bruh fizzy(smol n) do
+        count (smol i be no cap rn i lowkey_be n rn i glowup) do
+                sus (i % 3 do_be 0) do
+                        clapback("Fizz") rn
+                fr
+                sus (i % 5 do_be 0) do
+                        clapback("Buzz") rn
+                fr
+                sus (i % 3 no_be 0 uh i % 5 no_be 0) do
+                        clapback("%i", i) rn
+                fr
+                clapback("\n") rn
+        fr
+fr
+
+
+GOAT do
+        // see blud's mood do
+        smol mood[5] be "    " rn
+        tea("%4s", mood) rn
+        clapback("mood do be %s rn\n", mood) rn
+        
+        sus (comprable("good", mood) do_be cap) do
+                say("yo that's cool bro ngl!") rn
+        fr sussy (comprable("fine", mood) do_be cap) do
+                say("yo bro I gotchu!") rn
+                count (smol i be cap rn i lowkey 11 rn i glowup) do
+                         clapback("bro fr be a %i\n", i) rn
+                fr
+        fr baka do
+               say("yo, what?") rn
+        fr
+
+        // count blud's mood hash do
+        smol tote = 0 rn
+        count (smol i be longboy(mood) rn i really cap rn i glowdown) do
+                tote be tote + mood[i] rn
+        fr
+        yeet (tote) do
+                vibe 260 finna
+                        say("bro found secret yuh!") rn
+                        boop rn
+                vibe 420 finna
+                        say("dank!") rn
+                        boop rn
+                vibe 666 finna
+                        say("bro be trippin rn frfr!") rn
+                        boop rn
+                doot finna
+                        say("cool story bro...") rn
+        fr
+
+        // a suBROutine for all my bros
+        say("here comes the fizzy!") rn
+        fizzy(20) rn
+
+        // finna be finished here bro
+        dab no cap rn
+fr
+*/
