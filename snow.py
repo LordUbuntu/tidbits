8790dc1828e09f43560ff1530805dca331112000
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-27 22:23:41 -0800

    added snow program. Merry Christmass!

--- snow.py
+++ snow.py
@@ -2,16 +2,40 @@
 # Just for fun, based on a video from Engineer Man
 import os
 import random
-import time
+from time import sleep
 
 DENSITY = 5
 DELAY = 0.3
 
 flakes = ['λ', '•', '.', '❆', '❅', '❄', '*']
+width, height = os.get_terminal_size()
+# initialize display to all empty characters
+grid = [[' '] * width for _ in range(height)]
 
-# get terminal width
-# for ever
-    # create a string `s` that is `width` wide with `DENSITY` random `flakes` in it.
-    # concatenate that to the current string
-    # print it
-# This will cause the flakes to "fall" using terminal wrapping. Building the display by row.
+
+def draw_grid():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\033[?25l')  # ANSI clear cursor code
+    display = ''
+    for row in grid:
+        display += ''.join(row) + '\n'
+    display = display.strip('\n')
+    print(display, end='')
+
+
+while True:
+    # draw grid
+    draw_grid()
+
+    # scroll everything down once
+    row = []
+    for _ in range(width):
+        if random.random() < DENSITY / 100:
+            row.append(random.choice(flakes))
+        else:
+            row.append(' ')
+    grid.insert(0, row)
+    grid.pop()
+
+    # wait
+    sleep(DELAY)
