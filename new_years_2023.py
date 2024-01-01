f6132691bec648437c32a4dc5e8f6be463a19884
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-31 23:59:35 -0800

    added all needed libraries

--- new_years_2023.py
+++ new_years_2023.py
@@ -1,6 +1,11 @@
 # Jacobus Burger (2023)
 # Happy New Years (2023->2024)!
+from time import sleep
+from random import choice
+from colorama import Fore  # color text
 
+
+colors = [Fore.red, Fore.green, Fore.blue]
 message = "Happy New Years!"
 frames = [
     """
@@ -106,3 +111,7 @@ frames = [
          
     """,
 ]
+
+
+if __name__ == "__main__":
+    
