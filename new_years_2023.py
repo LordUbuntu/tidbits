e10b77341144faf8a696bb29b0d180d4cc3fb716
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-01 00:09:08 -0800

    completed, HAPPY NEW YEARS!!!!!!!

--- new_years_2023.py
+++ new_years_2023.py
@@ -1,11 +1,11 @@
 # Jacobus Burger (2023)
 # Happy New Years (2023->2024)!
 from time import sleep
+from os import name, system
 from random import choice
-from colorama import Fore  # color text
 
-
-colors = [Fore.red, Fore.green, Fore.blue]
+def clear(): system("cls" if name == "nt" else "clear")
+DELAY = 0.5
 message = "Happy New Years!"
 frames = [
     """
@@ -110,8 +110,22 @@ frames = [
          
          
     """,
+    """
+
+
+
+
+
+
+
+    """,
 ]
 
 
 if __name__ == "__main__":
-    
+    for frame in frames:
+        clear()
+        print(message)
+        print(frame)
+        sleep(DELAY)
+    input()
