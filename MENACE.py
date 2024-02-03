d0cd4e9bffb40b9860f06e369c845d771ec15516
Jacobus Burger <therealjacoburger@gmail.com>
2024-02-03 14:13:18 -0800

    added board and board visualization

--- MENACE.py
+++ MENACE.py
@@ -7,6 +7,12 @@ from os.path import exists
 from random import randint as rand
 import json  # for persistent memory
 
+
+def print_board(board):
+    for row in board:
+        print('|'.join(row))
+        print("-+-+-")
+
 # check if memory exists and load it in
 if exists("matchboxes.json"):
     with open("matchboxes.json", "r") as file:
@@ -16,18 +22,22 @@ else:
     matchboxes = [[rand(0, 8) for i in range(9)]]
 
 # start the game of tic-tac-toe
+board = ["   ", "   ", "   "]  # _, X, or O
 running = True
 move = 0
+print(matchboxes, board)  # DEBUG
 while running:
+    # show board
+    print_board(board)
     # menace goes first as O
     O = matchboxes[move][rand(0, 8)]
     move += 1
     # update board
+    board[O] = 'O'
+    print_board(board)
     # player goes next
 
 
 # overwrite matchbox memory
 with open("matchboxes.json", "w") as file:
     json.dump(matchboxes, file)
-
-print(matchboxes)
