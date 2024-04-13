ea815c8672a789c257c0b45e8d0d31da6a35f776
Jacobus Burger <therealjacoburger@gmail.com>
2024-04-12 20:01:48 -0700

    decided to go with it as an interpreter instead

--- calculator.py
+++
@@ -1,7 +0,0 @@
-def calculate(s):
-    # todo: safety checks so this isn't just an interpreter
-    return eval(s)
-
-
-if __name__ == '__main__':
-    print(calculate(input()))
---
+++ interpreter.py
@@ -0,0 +1,6 @@
+def interpret(s):
+    return eval(s)
+
+
+if __name__ == '__main__':
+    print(interpret(input()))
