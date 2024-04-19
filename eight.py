4ba13b43f3bc55e0a8dd6d0456a9248a8a359f37
Jacobus Burger <therealjacoburger@gmail.com>
2024-04-19 14:23:23 -0700

    added fortunes

--- eight.py
+++ eight.py
@@ -2,6 +2,21 @@
 # Magic eight ball on the command line!
 from color50 import rgb, constants
 
+FORTUNE = [
+    "It is certain",
+    "It is in the cards",
+    "Yes",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "Outlook not so good",
+    "Very doubtful"
+]
 
 
 def eightball(question: str) -> str:
