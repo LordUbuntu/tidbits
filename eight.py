f1181851abcd304082123770a4efae60dafa0e1c
Jacobus Burger <therealjacoburger@gmail.com>
2024-04-18 22:08:06 -0700

    added magic eight ball program (to become a package later)

---
+++ eight.py
@@ -0,0 +1,10 @@
+# Jacobus Burger (2024)
+# Magic eight ball on the command line!
+
+def eightball(question: str) -> str:
+    pass
+
+
+if __name__ == "__main__":
+    question = input()
+    eightball(question)
