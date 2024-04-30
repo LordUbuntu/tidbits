b94f6ea7c5f9079164c6494935b2d6a4aa8a3e92
Jacobus Burger <therealjacoburger@gmail.com>
2024-04-30 14:46:50 -0700

    added a new trick

--- tricks.py
+++ tricks.py
@@ -1,6 +1,10 @@
 # a collection of tricks I've made in Python
 
 
-def finish(generator):
+def finish(generator) -> list:
     """Instantiate all values in a generator into a list"""
     return [*generator]
+
+def flatten(L: list) -> list:
+    """Flattens a nested list down one dimension"""
+    return [*l for l in L]
