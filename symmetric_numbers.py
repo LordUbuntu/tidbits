c73b755dcc8ca87160b6ace6951aad5cf32a7555
Jacobus Burger <therealjacoburger@gmail.com>
2024-05-19 15:49:40 -0700

    started new tidbit to find numeric geometric annagrams

---
+++ symmetric_numbers.py
@@ -0,0 +1,39 @@
+# Jacobus Burger (2024)
+# I wondered if there was an algorithm to calculate if a number is 
+#   geometrically symmetric with itself, effectively a geometric numeric
+#   annagram while entertaining the idea with T.
+# This was determined to be a matter of matching numbers of symmetric
+#   opposites, splitting the number into two halves down its middle
+#   (including its middle digit), and then reversing the second half
+#   first in digit order, then replacing digits with their symmetric
+#   opposites, and then finally comparing that with the first half for
+#   a match.
+
+# list of symmetric opposites
+# notice that some numbers are their own symmetric opposites. This
+# relation could be graphed into a FSA
+sym = {
+    0: 0,
+    1: 1,
+    2: 5,
+    5: 2,
+    6: 9,
+    8: 8,
+    9: 6
+}
+
+
+def cleave(n):
+    """split a number into two numbers down the middle"""
+    s = str(n)
+    if len(s) % 2 == 0:
+        a = s[:len(s) // 2]
+        b = s[len(s) // 2:]
+    else:
+        a = s[:len(s) // 2 + 1]
+        b = s[len(s) // 2:]
+    return [int(a), int(b)]
+
+
+def is_symmetric(n):
+    pass
