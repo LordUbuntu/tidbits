095958aa81425a48781c60e29386dec4432f9ab5
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-27 00:08:20 -0700

    fixed spacing

--- panagram.py
+++ panagram.py
@@ -3,20 +3,21 @@
 from random import choice
 
 def basic_panagram():
-  PANAGRAMS = [
-    "Waltz, bad nymph, for quick jigs vex.",
-    "Glib jocks quiz nymph to vex dwarf.",
-    "Sphinx of black quartz, judge my vow.",
-    "How quickly daft jumping zebras vex!",
-    "The five boxing wizards jump quickly.",
-    "Jackdaws love my big sphinx of quartz.",
-    "Pack my box with five dozen liquor jugs.",
-    "Amazingly few discotheques provide jukeboxes.",
-    "Brawny gods just flocked up to quiz and vex him.",
-    "The quick onyx goblin jumps over the lazy dwarf.",
-    "Six big devils from Japan quickly forgot how to waltz.",
-  ]
-  return choice(PANAGRAMS)
+    
+    PANAGRAMS = [
+        "Waltz, bad nymph, for quick jigs vex.",
+        "Glib jocks quiz nymph to vex dwarf.",
+        "Sphinx of black quartz, judge my vow.",
+        "How quickly daft jumping zebras vex!",
+        "The five boxing wizards jump quickly.",
+        "Jackdaws love my big sphinx of quartz.",
+        "Pack my box with five dozen liquor jugs.",
+        "Amazingly few discotheques provide jukeboxes.",
+        "Brawny gods just flocked up to quiz and vex him.",
+        "The quick onyx goblin jumps over the lazy dwarf.",
+        "Six big devils from Japan quickly forgot how to waltz.",
+    ]
+    return choice(PANAGRAMS)
 
 
 if __name__ == '__main__':
