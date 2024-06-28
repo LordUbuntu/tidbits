12177b07d710f217940e81391764c6c6f761e53c
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-28 00:03:48 -0700

    added joke random number generator

---
+++ rnjoke.py
@@ -0,0 +1,13 @@
+# Jacobus Burger (2024)
+# I had a funny idea to make an RNG that selects a random digit of PI
+#   using Python's builtin RNG to do so.
+from math import pi
+from random import choice
+
+
+def rng():
+    digits = [int(digit) if digit != '.' else 0 for digit in str(pi)]
+    return choice(digits)
+
+
+print(rng())
