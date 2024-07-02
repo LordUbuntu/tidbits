0f1a30909f6b772df5cc735e4c859f3be171247c
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-02 14:03:23 -0700

    remove redundant func

--- func.c
+++ func.c
@@ -34,9 +34,6 @@ int mul(int a, int b) {
 int div(int a, int b) {
         return a / b;
 }
-int product(int a, int b) {
-        return a * b;
-}
 
 // higher order functions
 void filter(bool (*f)(int), int *A, int *B, size_t length) {
