039761b998721d423648103f110c91e5c43b2bce
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-03 20:49:46 -0700

    added a new program

---
+++ sick.c
@@ -0,0 +1,3 @@
+// Jacobus Burger (2024-07-04)
+// A basic program that appends "achu" onto a word. To commemorate getting sick from 2024-07-02 to 2024-07-05. Written in C because I haven't done a lot in C recently.
+
