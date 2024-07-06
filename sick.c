7f2990af6e469751a8198064ae7c0fac59e54e1d
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-05 23:18:54 -0700

    incorporated user defined sound feature

--- sick.c
+++ sick.c
@@ -1,16 +1,17 @@
 // Jacobus Burger (2024-07-04)
-// A basic program that appends "achu" onto a word. To commemorate getting sick from 2024-07-02 to 2024-07-05. Written in C because I haven't done a lot in C recently.
+// A basic program that appends "achu" onto a sound. To commemorate getting sick from 2024-07-02 to 2024-07-05. Written in C because I haven't done a lot in C recently.
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 
 void help_info() {
-        puts("Usage: sick [OPTION] ... [WORDS] ...");
-        puts("Adds sneezing sounds to the end of a sentence or each word.");
+        puts("Usage: sick [OPTION] ... [soundS] ...");
+        puts("Adds sneezing sounds to the end of a sentence or each sound.");
         puts("");
         puts("With no OPTION, will only add an 'achu' at the end of sentence.");
-        puts("  -e, --every     adds a sneeze to the end of every word");
+        puts("  -e, --every     adds a sneeze to the end of every sound");
+        puts("  -s, --sound     define the sneeze sound at the end of sounds");
 }
 
 int main(int argc, char *argv[])
@@ -22,25 +23,25 @@ int main(int argc, char *argv[])
         }
         // process args
         bool every = false;
-        string word = "achu";
+        string sound = "achu";
         for (int i = 1; i < argc; i++) {
                 if (strcmp(argv[i], "--every") == 0 || strcmp(argv[i], "-e") == 0)
                         every = true;
                 // set new sound if defined
-                if (strcmp(argv[i], "--word") == 0 || strcmp(argv[i], "-w") == 0) {
-                        word = i + 1 < argc ? argv[i + 1] : "";
-                if (strcmp(word, "") == 0) {
+                if (strcmp(argv[i], "--sound") == 0 || strcmp(argv[i], "-s") == 0) {
+                        sound = i + 1 < argc ? argv[i + 1] : "";
+                if (strcmp(sound, "") == 0) {
                         help_info();
                         return;
                 }
         }
         if (every) {
                 for (int i = 2; i < argc; i++)
-                        printf("%s%s ", argv[i], word);
+                        printf("%s%s ", argv[i], sound);
         } else {
                 for (int i = 1; i < argc - 1; i++)
                         printf("%s ", argv[i]);
-                printf("%s%s ", argv[argc - 1], word);
+                printf("%s%s ", argv[argc - 1], sound);
         }
         puts("*sniffle*");
 }
