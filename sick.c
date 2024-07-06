d0d98b512d3a1431ec88c58eb45476a0d00238ba
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-05 23:10:19 -0700

    changing the way args are processed

--- sick.c
+++ sick.c
@@ -2,6 +2,7 @@
 // A basic program that appends "achu" onto a word. To commemorate getting sick from 2024-07-02 to 2024-07-05. Written in C because I haven't done a lot in C recently.
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 void help_info() {
@@ -16,10 +17,13 @@ void help_info() {
 int main(int argc, char *argv[])
 {
         // show help info
-        if (argc <= 1 \
-        ||  strcmp(argv[1], "--help") == 0 \
-        ||  strcmp(argv[1], "-h") == 0) {
+        if (argc <= 1 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
                 help_info();
+        }
+        // process args
+        bool every = false;
+        string word = "achu";
+        for (int i = 1; i < argc; i++) {
         }
         if (strcmp(argv[1], "--every") == 0 || strcmp(argv[1], "-e") == 0) {
                 for (int i = 2; i < argc; i++)
