0992b9be158a889302c3395ec075cc37a94a8b0c
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-05 22:44:03 -0700

    added a --help command

--- sick.c
+++ sick.c
@@ -8,6 +8,16 @@ int main(int argc, char *argv[])
         if (argc < 1) {
                 puts("~ACHU~");
         }
+        if (argc == 1 \
+        ||  strcmp(argv[1], "--help") == 0 \
+        ||  strcmp(argv[1], "-h") == 0) {
+                puts("Usage: sick [OPTION] ... [WORDS] ...");
+                puts("Adds sneezing sounds to the end of a sentence or each word.");
+                puts("");
+                puts("With no OPTION, will only add an 'achu' at the end of sentence.");
+                puts("  -e, --every     adds a sneeze to the end of every word");
+                return;
+        }
         if (strcmp(argv[1], "--every") == 0 || strcmp(argv[1], "-e") == 0) {
                 for (int i = 2; i < argc; i++)
                         printf("%sachu ", argv[i]);
