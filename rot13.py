de2ecdb82bb967cb59a277e70cbd9da3c4cd4aea
Jacobus Burger <therealjacoburger@gmail.com>
2024-08-22 23:47:35 -0700

    added a rot function that can rotate cypher by n positions

--- rot13.py
+++ rot13.py
@@ -14,9 +14,13 @@ def rot13(data: str) -> str:
     return ''.join(ascii_lowercase[(i + 13) % 26] for i in map(ascii_lowercase.index, data.lower()))
 
 
-def rotN(data: str) -> str:
+def rot_rand(data: str) -> str:
     N = random(1, 100)
     return ''.join(ascii_lowercase[(i + N) % 26] for i in map(ascii_lowercase.index, data.lower()))
+
+
+def rot(data: str, n: int = 13) -> str:
+    return ''.join(ascii_lowercase[(i + n) % 26] for i in map(ascii_lowercase.index, data.lower()))
 
 
 if __name__ == '__main__':
