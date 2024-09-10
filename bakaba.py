e445a52990e4b05ba5882bbeae9ddb0a79343dd1
Jacobus Burger <therealjacoburger@gmail.com>
2024-09-09 20:48:29 -0700

    move TODO out of function

--- bakaba.py
+++ bakaba.py
@@ -9,8 +9,8 @@
 #   here is my attempt at that.
 
 
+# TODO: replace to recognize pattern using sliding window method instead
 def bakaba(repetitions, string):
-    # TODO: replace to recognize pattern using sliding window method instead
     cap = string[0:2]
     pattern = string[2 : len(string) - 2]
     return cap + (pattern * repetitions) + cap
