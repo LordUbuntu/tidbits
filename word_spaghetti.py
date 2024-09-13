93268f622bdb5775f325e9323518f6fdd53b9ed7
Jacobus Burger <therealjacoburger@gmail.com>
2024-09-12 23:15:52 -0700

    plans to add speghetti

---
+++ word_spaghetti.py
@@ -0,0 +1,6 @@
+# Jacobus Burger (2024-09-12)
+# generates a string of random words of a random length from a text file
+# inspired by oracles, a fascination with procedural generation, and terry davis
+
+
+# TODO:
