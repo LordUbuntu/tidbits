dbbfc99410c31766be06a63c780a52d831e77171
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-26 23:15:04 -0700

    made plans for the 30th

---
+++ halloween_2024.py
@@ -0,0 +1,6 @@
+# Jacobus Burger (Oct 30, 2024)
+# A fun tradition I do every Halloween, like how "Grinch's Ultimatum" is
+#   a tradition for Christmas.
+# This year, the "Monster Mash" song inspired me to make an animated
+#   kereoke of the lyrics.
+# TODO:
