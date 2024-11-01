d225ca12d544a8b1d58f76aa3f5f46895cd13a63
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-31 20:42:15 -0700

    data prepared for ~~monster mash~~

--- halloween_2024.py
+++ halloween_2024.py
@@ -1,6 +1,29 @@
 # Jacobus Burger (Oct 30, 2024)
 # A fun tradition I do every Halloween, like how "Grinch's Ultimatum" is
 #   a tradition for Christmas.
-# This year, the "Monster Mash" song inspired me to make an animated
-#   kereoke of the lyrics.
-# TODO:
+# This year, the "Monster Mash" song inspired me to make a cute little
+#   dance and lyrics thing for it from ascii
+from time import sleep
+
+
+lyrics = [
+    "(It did the mash)",
+    "It did the ~monster mash~",
+    "(A graveyard smash)",
+    "It was a ~graveyard smash~"
+]
+
+animation = [
+    """
+    <(._.<)
+    """,
+    """
+    <(._.)>
+    """,
+    """
+    (>._.)>
+    """,
+    """
+    <(._.)>
+    """
+]
