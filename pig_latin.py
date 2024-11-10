f8ef107d48910ac5251f1e24d90f1bed68bd54cf
Jacobus Burger <therealjacoburger@gmail.com>
2024-11-09 20:54:58 -0800

    theory

--- pig_latin.py
+++ pig_latin.py
@@ -2,6 +2,7 @@
 # A program that converts a normal sentence into pig latin
 
 def pig(sentence: str) -> str:
+    # hey how are you -> eyhay owhay arey ouyay
     return ' '.join(word[1:]+word[0]+"ay" for word in sentence.split())
 
 
