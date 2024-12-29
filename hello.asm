bf47149400f1e1141c0c4a2164033a7dea87677f
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-28 16:57:39 -0800

    change: comments are simpler

--- hello.asm
+++ hello.asm
@@ -1,10 +1,10 @@
 ; Created by Jacobus Burger (2019)
 ;
 ; Info:
-;		One of my first programs. The very first was action-script (man I'm old).
-;		Just a bog-standard hello world program written in assembly.
+;	One of my first programs. The very first was action-script
+;       (man am I old)
+;       Just a standard hello world program written in assembly.
 section .data
-        ; NOTE: 10 is \n, 0 is \0
 	msg db "你好 世界!", 10, "Bonjour le monde!", 10, "Hello world!", 10, "Hola mundo!", 10, 10, 0
 	len equ $ - msg
 
