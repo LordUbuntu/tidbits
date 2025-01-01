425daafc5fe5fe4b4654d64a4b45d724b9353ac9
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-31 21:20:23 -0800

    add: new years 2024

---
+++ new_years_2024.py
@@ -0,0 +1,5 @@
+# Jacobus Burger (2024->2025)
+# This new years program prints ascii simulated fire using a similar
+#   principle to the winter snowfall program `snow.py`. Inspired by
+#   a desire to come on a year anew like a phoenix (🐦‍🔥) and
+#   "keep that fire alive"
