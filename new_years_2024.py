eaf958e2295ef9852e3be3b6539e883cc86bc2b4
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-31 21:51:04 -0800

    wrote theory and what functions will be needed

--- new_years_2024.py
+++ new_years_2024.py
@@ -3,3 +3,20 @@
 #   principle to the winter snowfall program `snow.py`. Inspired by
 #   a desire to come on a year anew like a phoenix (🐦‍🔥) and
 #   "keep that fire alive"
+import os
+from time import sleep
+from collections import deque
+
+
+def clear(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# I want this to run whether imported or called as a script
+while True:
+    # clear screen
+    # add a row on the back of the grid queue (bottom of display)
+    #   in that row, add elements in a normal (bell curve) distribution
+    #   across the terminal width (more likely in middle than edges)
+    # iterate through each row
+    #   update "pixel" to be "colder" as it gets higher
