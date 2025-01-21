0e2bc300c4b98934c015048920b0cc2fe1142dab
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-20 23:08:02 -0800

    never give up 🐍

---
+++ give_up.py
@@ -0,0 +1,18 @@
+# Jacobus Burger (2025-01-20)
+# Life can feel like a lot sometimes, like it's beating you down.
+# But life is a gift, a dance and process we experience for ourselves.
+# So be stubborn like me 🐍. Don't let trouble weigh you down, but fly!
+from random import choice
+
+def never_give_up():
+    inspiration = [
+        "Was mich nicht umbringt, macht mich stärker\n(What does not kill me, makes me stronger)",
+        "七転び八起き\n(fall down seven times, get up eight)",
+        "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)",
+        "Aut inveniam viam aut faciam\n(Either I will find a way or I will make on)",
+        "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)",
+        "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it",
+    ]
+    print(choice(inspiration))
+
+never_give_up()
