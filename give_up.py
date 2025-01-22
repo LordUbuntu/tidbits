25a676ca13bcd693082294147cbfe54d5185a2d9
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-21 16:45:51 -0800

    add more inspiration, never give up 🐍!

--- give_up.py
+++ give_up.py
@@ -9,9 +9,11 @@ def never_give_up():
         "Was mich nicht umbringt, macht mich stärker\n(What does not kill me, makes me stronger)",
         "七転び八起き\n(fall down seven times, get up eight)",
         "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)",
-        "Aut inveniam viam aut faciam\n(Either I will find a way or I will make on)",
+        "Aut inveniam viam aut faciam\n(Either I will find a way or I will make one)",
         "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)",
         "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it",
+        "諦めんな絶対にがんばれ積極的にポジティブにがんばれがんばれ！.\n(Don't give up, always do your best, be positive and do your best!)",
+        "ἐν τῷ κόσμῳ θλῖψιν ἔχετε. ἀλλὰ θαρσεῖτε; ἐγὼ νενίκηκα τὸν κόσμο\n(In the world you will have tribulation. But take heart; I have overcome the world)"
     ]
     print(choice(inspiration))
 
