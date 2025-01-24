c3dd41789579053bc85de2f4bb45259c1cdc7836
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-23 23:24:40 -0800

    added marie curie credits

--- give_up.py
+++ give_up.py
@@ -10,7 +10,7 @@ def never_give_up():
         "七転び八起き\n(fall down seven times, get up eight)",
         "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)\n- Confucious",
         "Aut inveniam viam aut faciam\n(Either I will find a way or I will make one)- Hannibal",
-        "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)",
+        "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)\n- Marie Curie",
         "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it",
         "諦めんな絶対にがんばれ積極的にポジティブにがんばれがんばれ！.\n(Don't give up, always do your best, be positive and do your best!)",
         "ἐν τῷ κόσμῳ θλῖψιν ἔχετε. ἀλλὰ θαρσεῖτε; ἐγὼ νενίκηκα τὸν κόσμο\n(In the world you will have tribulation. But take heart; I have overcome the world)"
