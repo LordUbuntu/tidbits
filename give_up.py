fb7b037a0f87086329d6de83e4a80c458b672e3a
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-23 23:24:15 -0800

    added confucious credits

--- give_up.py
+++ give_up.py
@@ -8,7 +8,7 @@ def never_give_up():
     inspiration = [
         "Aus der Kriegsschule des Lebens - Was mich nicht umbringt, macht mich stärker\n(Out of life’s school of war - What does not kill me, makes me stronger)\n- Aphorism 8 from \"Maxims and Arrows\" in \"Twilight of the Idols\", Freidrich Nietzsche",
         "七転び八起き\n(fall down seven times, get up eight)",
-        "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)",
+        "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)\n- Confucious",
         "Aut inveniam viam aut faciam\n(Either I will find a way or I will make one)",
         "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)",
         "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it",
