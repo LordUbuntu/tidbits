bf56312ce3700ffa11a9bf23075b062151af5d9d
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-24 18:44:32 -0800

    complete credits

--- give_up.py
+++ give_up.py
@@ -7,12 +7,12 @@ from random import choice
 def never_give_up():
     inspiration = [
         "Aus der Kriegsschule des Lebens - Was mich nicht umbringt, macht mich stärker\n(Out of life’s school of war - What does not kill me, makes me stronger)\n- Aphorism 8 from \"Maxims and Arrows\" in \"Twilight of the Idols\", Freidrich Nietzsche",
-        "七転び八起き\n(fall down seven times, get up eight)",
+        "七転び八起き\n(fall down seven times, get up eight)\n- Japanese Proverb",
         "走得慢不要緊，最重要的是不要停。\n(It doesn't matter if you walk slowly, the most important thing is not to stop)\n- Confucious",
         "Aut inveniam viam aut faciam\n(Either I will find a way or I will make one)- Hannibal",
         "On m'a appris que le chemin du progrès n'est ni rapide ni facile\n(I was taught that the path to progress is neither quick nor easy)\n- Marie Curie",
         "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it\n",
-        "諦めんな絶対にがんばれ積極的にポジティブにがんばれがんばれ！.\n(Don't give up, always do your best, be positive and do your best!)",
+        "諦めんな絶対にがんばれ積極的にポジティブにがんばれがんばれ！.\n(Don't give up, always do your best, be positive and do your best!)\n- Matsuoka Shuzo",
         "ἐν τῷ κόσμῳ θλῖψιν ἔχετε. ἀλλὰ θαρσεῖτε; ἐγὼ νενίκηκα τὸν κόσμο\n(In the world you will have tribulation. But take heart; I have overcome the world)\n- John 16:33"
     ]
     print(choice(inspiration))
