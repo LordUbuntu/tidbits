e166b328f8d9c6f8091da1c96c66d685b1745e0b
Jacobus Burger <therealjacoburger@gmail.com>
2025-02-09 20:37:29 -0800

    create start of github crawler

---
+++ github_crawler.py
@@ -0,0 +1,6 @@
+# Jacobus Burger (2025-02-09)
+# A web crawler to explore and graph social connections on GitHub from
+#   a given user page for a specified graph depth, run in parallel
+#   for faster performance.
+# The start of a larger project that will get it's own repo once I figure
+#   out a better name for it and have more time.
