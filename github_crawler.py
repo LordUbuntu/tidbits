196242e8ec110be139291cb5fecffaf7c50b4506
Jacobus Burger <therealjacoburger@gmail.com>
2025-02-10 23:05:00 -0800

    add some feature ideas

--- github_crawler.py
+++ github_crawler.py
@@ -4,3 +4,11 @@
 #   for faster performance.
 # The start of a larger project that will get it's own repo once I figure
 #   out a better name for it and have more time.
+
+# Features:
+# - command line utility
+# - specify depth and other features to explore relational graphs
+# - visualize graphs
+# - parallelism for fast performance
+# - basic API interaction with GitHub to be easy to use and change
+
