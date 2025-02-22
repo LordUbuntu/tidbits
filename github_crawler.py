fe2c581681414dc18f073c3cf6098c04495d5363
Jacobus Burger <therealjacoburger@gmail.com>
2025-02-21 23:21:39 -0800

    new feature ideas planned

--- github_crawler.py
+++ github_crawler.py
@@ -4,17 +4,19 @@
 #   for faster performance.
 # The start of a larger project that will get it's own repo once I figure
 #   out a better name for it and have more time.
+# TODO: I really need to come up with a name for this program
 
 # Features:
 # - command line utility
 # - specify depth and other features to explore relational graphs
-# - visualize graphs
+# - visualize graphs (with graphviz or pyviz output options)
+# - record and store network links as .csv and other data formats
 # - parallelism for fast performance
 # - basic API interaction with GitHub to be easy to use and change
 import argparse                 # CLI
 import multiprocessing as mp    # multiprocessing for faster execution
 # consider other options for interactive and fast graph network
-#   visualizations, like pyviz, networkx, sigma.js, etc
+#   visualizations, like pyvis, networkx, etc
 import graphviz as gv
 import requests as req          # getting requests from site
 import bs4                      # parsing site
