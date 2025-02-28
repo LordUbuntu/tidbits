806f526f1a70f0dd899a63493f3898392927f7ae
Jacobus Burger <therealjacoburger@gmail.com>
2025-02-27 22:47:16 -0800

    added a note about big plans for this little script

--- text_effects.py
+++ text_effects.py
@@ -2,4 +2,5 @@
 # Thought about and imagined a program made to do all sorts of effects
 #   on bits of text in a terminal.  I'll take the vapourtext program
 #   and others and combine them, add new ones, and more
+# This will become a library for TUI. I'll make it's own repo later
 
