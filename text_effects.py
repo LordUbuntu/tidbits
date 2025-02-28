5ac7409831ebde0491fe843224de31acd3260295
Jacobus Burger <therealjacoburger@gmail.com>
2025-02-27 22:46:25 -0800

    added text effects

---
+++ text_effects.py
@@ -0,0 +1,5 @@
+# Jacobus Burger (2025-02-28)
+# Thought about and imagined a program made to do all sorts of effects
+#   on bits of text in a terminal.  I'll take the vapourtext program
+#   and others and combine them, add new ones, and more
+
