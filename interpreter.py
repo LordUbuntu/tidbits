2f4146965f9536c50d223be1b8a657b54967c71e
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-02 21:32:03 -0800

    going to make a proper interpreter soon

--- interpreter.py
+++ interpreter.py
@@ -1,11 +1,3 @@
 # This could be improved
 # I should rework this into a simplified Python 3 subset parser and
 #   interpreter inferface.
-
-
-def interpret(s):
-    return eval(s)
-
-
-if __name__ == '__main__':
-    print(interpret(input()))
