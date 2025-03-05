53e53401ecf9e85aad94760d0891d58a2ed3aec1
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-04 23:01:26 -0800

    wrapping this one up

--- job.py
+++ job.py
@@ -15,3 +15,9 @@ while True:
 
 # Jacobus Burger (???)
 # Wrote a script in first week that's very helpful, multiprocessing
+
+# Jacobus Burger (...)
+# add more points here...
+
+# Jacobus Burger (2025-03-04)
+# LST WK
