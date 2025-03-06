d09d2a469a5102cb90067f38c882949dddd44324
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-05 21:56:34 -0800

    begin small work on stack machine interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,12 @@
 # This could be improved
 # I should rework this into a simplified Python 3 subset parser and
 #   interpreter inferface.
+# I'll make this a stack machine interpreter to calculate maths in
+#   polish (prefix) notation. (remember shunting yard to show as infix)
+
+class StackMacine:
+    def __init__(self):
+        self.stack = []
+
+
+interpreter = StackMachine()
