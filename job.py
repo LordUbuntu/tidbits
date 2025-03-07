94012a1819dbb5fb93474283e2cc5977910e4c3d
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-06 22:22:43 -0800

    prep to remove

--- job.py
+++ job.py
@@ -1,12 +1,6 @@
 # Jacobus Burger (2025-01-30)
 # Got an internship today!
 
-while True:
-    print("JOB")
-    sleep(1)
-    print("GET")
-    sleep(1)
-
 # Jacobus Burger (2025-02-03)
 # Signed contracts and started
 
