2efdc6ced2b6eb0c66781dba9cf52bbafa0e4038
Jacobus Burger <therealjacoburger@gmail.com>
2025-03-16 22:11:31 -0700

    add random capitalization LiKe ThIS

--- text_effects.py
+++ text_effects.py
@@ -3,7 +3,12 @@
 #   on bits of text in a terminal.  I'll take the vapourtext program
 #   and others and combine them, add new ones, and more
 # This will become a library for TUI. I'll make it's own repo later
+from random import randint
 
 
 def vapourwave(sentence):
     return ' '.join([*sentence])
+
+
+def rcaps(sentence):
+    return ''.join([char.upper() if randint(0, 1) else char for char in sentence])
