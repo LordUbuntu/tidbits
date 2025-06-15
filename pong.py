db9689358c71f5f806b7eb8a76e686ec31785948
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-14 20:32:03 -0700

    begin implementing pong in ncurses

--- pong.py
+++ pong.py
@@ -1,37 +1,56 @@
-# Jacobus Burger (2025-06-12)
-# Random Ball in Terminal
-import keyboard  # dependency
-import os
-import random
-from time import sleep
+#!/bin/python3
+# Created by Jacobus Burger (2025-06-14)
+# Info:
+#   Play the game Pong in terminal!
+# See:
+#   https://en.wikipedia.org/wiki/Pong
+from sys import argv
+from os import system
+from random import randint as rand
+import curses
 
 
-
-def clear():
-    os.system("cls" if os.name == "nt" else "clear")
-
-width, height = os.get_terminal_size()
-screen = [' ' * width] * height
-PONG = "*"
-TIMEOUT = 1
+def pong():
+    pass
 
 
-try:
-    print('\033[?25l', end='')  # hide cursor
-    while True:
-        # refresh screen
-        clear()
+if __name__ == '__main__':
+    # get the update interval from the command line
+    speed = int(argv[1]) if len(argv) > 1 else 100
 
-        # update ball position
+    # initialize ncurses
+    stdscr = curses.initscr()
+    curses.curs_set(False)
+    curses.noecho()
+    curses.cbreak()
+    curses.start_color()
 
-        # update paddle position from input
+    # set up colors
+    blank_color = 1
+    blank_char = " "
+    ball_color = 2
+    ball_char = "*"
+    paddle_color_A = 3
+    paddle_color_B = 4
+    paddle_char = "I"
+    curses.init_pair(blank_color, curses.COLOR_BLACK, curses.COLOR_BLACK)
+    curses.init_pair(ball_color, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(paddle_color_A, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(paddle_color_B, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
-        # display screen
-        for h in range(height):
-            print(screen[h])
+    # initialize game window
+    height, width = stdscr.getmaxyx()
+    win = curses.newwin(height, width, 0, 0)
+    win.keypad(True)
+    win.timeout(speed)
+    win.border()
 
-        # wait for next step
-        sleep(TIMEOUT)
-except KeyboardInterrupt:
-    print('\033[?25h', end='')  # reveal cursor again
-    clear()
+    # play game
+    pong()
+
+    # clean up and end
+    curses.curs_set(True)
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+    system("clear")
