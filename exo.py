d720555e078ac1589f1180678bb74fef793764c8
Jacobus Burger <therealjacoburger@gmail.com>
2025-09-03 20:08:10 -0700

    ITS THAT EXO!

---
+++ exo.py
@@ -0,0 +1,13 @@
+# Jacobus Burger (2025-09-03)
+# Fever The Ghost x Lealani
+
+
+if __name__ == "__main__":
+    for _ in range(13):
+        print("ITS THAT EXO!")
+    print("ITS THAT ", end='')
+    for _ in range(7):
+        print("EX", end='')
+    print("-")
+    print("EXOFLASH!!")
+    print("REVELATION!!!")
