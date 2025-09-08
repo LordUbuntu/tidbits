b357ae7740f0c184ef2eb718d79fffa00c9ad424
Jacobus Burger <therealjacoburger@gmail.com>
2025-09-08 14:22:59 -0700

    add: hissssing talkssss

---
+++ snaketalk.py
@@ -0,0 +1,13 @@
+# Jacobus Burger (2025-09-08)
+# In media they portray snakes speaking with protracted s'
+#   like thisssss, maybe they do, but I think it would be fun
+#   to write a quick string formatter for that.
+
+
+def ssspeak(phrase: str) -> str:
+    return phrase.replace('s', "ssss")
+
+
+if __name__ == "__main__":
+    phrase = input()
+    print(ssspeak(phrase))
